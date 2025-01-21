ddc239522399bc38ef2d8d987fbfc8cef1fc579e
Matteo Cencini <matteo.cencini@gmail.com>
2025-01-21 15:47:51 +0100

    WIP GE reader.

--- src/mrbabel/io/gehc/_read.py
+++ src/mrbabel/io/gehc/_read.py
@@ -1,69 +1,64 @@
 """GE HealthCare reading routines."""
 
-__all__ = ["read_siemens"]
+__all__ = ["read_gehc"]
 
 import warnings
 
 import mrd
-import twixtools
+
+try:
+    import getools
+    __GEHC_AVAILABLE__ = True
+except Exception:
+    __GEHC_AVAILABLE__ = False
 
 from ...data import sort_kspace
 from ..._file_search import get_paths
 
-from ..converters._siemens2mrd import read_siemens_header, read_siemens_acquisitions
+from ..converters._gehc2mrd import read_gehc_header, read_gehc_acquisitions
 
 
-def read_siemens(
+def read_gehc(
     path: str,
     sort: bool = True,
-    xml_file: str | None = None,
-    xsl_file: str | None = None,
 ) -> mrd.ReconBuffer | list[mrd.ReconBuffer] | list[mrd.Acquisition]:
     """
-    Read input Siemens k-space file.
+    Read input GE HealthCare k-space file.
 
     Parameters
     ----------
     paths : str | list of str
-        Path or list of file paths to Siemens files.
+        Path or list of file paths to GEHC files.
     sort : bool, optional
         If ``True``, sort list of MRD Acquisitions into a MRD ReconBuffer.
         The default is ``True``.
-    xml_file : str | None, optional
-        XML file to create xml string from Twix header. If not
-        provided, uses the same default as "siemens_to_ismrmrd".
-        The default is ``None``.
-    xsl_file : str | None, optional
-        XSLT file to convert Twix xml string to MRD. If not
-        provided, uses the same default as "siemens_to_ismrmrd".
-        The default is ``None``.
 
     Returns
     -------
     image : mrd.ReconBuffer | list[mrd.ReconBuffer] | list[mrd.Acquisition]
         MRD ReconBuffer(s)  or list of MRD Acquisitions parsed from MRD file.
     head : mrd.Head
-        MRD Header parsed from Siemens files.
+        MRD Header parsed from GEHC files.
 
     """
-    if isinstance(path, str) and path.endswith(".dat"):
+    if __GEHC_AVAILABLE__ is False:
+        print("GEHC reader is private - ask for access at https://docs.google.com/forms/d/1BvA1h8qb9GmndqiXMplQbf3IujgBIehQ1psnfmW0tew/edit")
+    if isinstance(path, str) and (path.endswith(".h5") or path.endswith(".7")):
         path = [path]
     else:
-        path = get_paths("dat", path)
+        path = get_paths("h5", path, "7")
     if len(path) == 0:
-        raise ValueError("Siemens file not found in target directory.")
+        raise ValueError("GEHC file not found in target directory.")
     if len(path) > 1:
         raise warnings.warn(
-            f"Found multiple ({len(path)}) dat files - picking the first", UserWarning
+            f"Found multiple ({len(path)}) PFiles/ScanArchive files - picking the first", UserWarning
         )
     path = path[0]
 
     # reading
-    twix_obj = twixtools.read_twix(path)
-    if isinstance(twix_obj, list):
-        twix_obj = twix_obj[-1]
-    head = read_siemens_header(twix_obj["hdr"])
-    acquisitions = read_siemens_acquisitions(twix_obj["hdr"], twix_obj["mdb"])
+    gehc_raw, gehc_head = getools.read_rawdata(path)
+    head = read_gehc_header(gehc_head)
+    acquisitions = read_gehc_acquisitions(head, gehc_raw)
 
     if sort:
         recon_buffers = sort_kspace(acquisitions, head)
