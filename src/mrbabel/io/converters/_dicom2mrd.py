Fixing scaling for dicom and niftis.

src/mrbabel/io/converters/_dicom2mrd.py
@@ -356,7 +356,7 @@ def read_dicom_images(
 
         # Initialize current image header
         image_head = mrd.ImageHeader(image_type=image_type)
-        
+
         # Flags
         defs = mrd.ImageFlags
         if slice_idx[n] == 0:
@@ -453,6 +453,48 @@ def read_dicom_images(
         except Exception:
             pass
 
+        # Get scaling and intercept
+        if "PHILIPS" in vendor.upper():
+            # from https://github.com/rordenlab/dcm2niix/tree/master/Philips
+            # here, we attempt to use, in order, FP, DV, WV to match dcm2niix
+            try:
+                WS = dset[0x0040, 0x9096][0][0x0040, 0x9225].value
+                WI = dset[0x0040, 0x9096][0][0x0040, 0x9224].value
+            except Exception:
+                WS = None
+                WI = None
+            try:
+                RS = dset[0x0040, 0x9096][0][0x0028, 0x1053].value
+                RI = dset[0x0040, 0x9096][0][0x0028, 0x1052].value
+            except Exception:
+                RS = None
+                RI = None
+            try:
+                SS = dset[0x0040, 0x9096][0][0x2005, 0x100E].value
+            except Exception:
+                SS = None
+
+            if RS is not None and RI is not None:
+                if SS is not None:
+                    scl_slope = 1 / float(SS)
+                    scl_inter = float(RI) / (float(RS) * float(SS))
+                else:
+                    scl_slope = float(RS)
+                    scl_inter = float(RI)
+            elif WS is not None and WI is not None:
+                scl_slope = float(WS)
+                scl_inter = float(WI)
+            else:
+                scl_slope = 1.0
+                scl_inter = 0.0
+        else:  # other vendors
+            try:
+                scl_slope = float(dset.RescaleSlope)
+                scl_inter = float(dset.RescaleIntercept)
+            except Exception:
+                scl_slope = 1.0
+                scl_inter = 0.0
+
         # Remove pixel data from pydicom class
         try:
             image_data = dset.pixel_array.copy().astype(np.float32)
@@ -476,6 +518,10 @@ def read_dicom_images(
                     del dset["DoubleFloatPixelData"]
                 except Exception:
                     image_data = None
+
+        # Rescale
+        if image_data is not None:
+            image_data = scl_slope * image_data + scl_inter
 
         # Store the complete base64, json-formatted DICOM header so that non-MRD fields can be
         # recapitulated when generating DICOMs from MRD images

src/mrbabel/io/converters/_nifti2dicom.py
@@ -63,15 +63,13 @@ def nifti2dicom(nii_paths: list[str], nii: list[Nifti1Image]) -> list[pydicom.Da
 
     # Get images
     img = [vol.get_fdata().astype(np.float32) for vol in nii]
-    
+
     # Get parameters from first volume
     idx = 0
     try:
         nii[idx].affine[:2] *= -1
         ndims = int(json[idx]["MRAcquisitionType"][0])
-        iop = np.asarray(json[idx]["ImageOrientationPatientDICOM"]).reshape(
-            2, 3
-        )
+        iop = np.asarray(json[idx]["ImageOrientationPatientDICOM"]).reshape(2, 3)
         scan_orient = detect_scan_orientation(iop)
         if ndims == 2:
             if scan_orient == "ax":
@@ -94,15 +92,13 @@ def nifti2dicom(nii_paths: list[str], nii: list[Nifti1Image]) -> list[pydicom.Da
     nii2dcm_parameters = nii2dcm.nii.Nifti.get_nii2dcm_parameters(nii[idx])
 
     # Fix wrong nii2dcm iop
-    iop = -1 * np.asarray(nii2dcm_parameters["ImageOrientationPatient"]).reshape(
-        2, 3
-    )
+    iop = -1 * np.asarray(nii2dcm_parameters["ImageOrientationPatient"]).reshape(2, 3)
     iop = np.flip(iop, axis=0).ravel()
     nii2dcm_parameters["ImageOrientationPatient"] = iop.tolist()
 
     # Fix wrong nii2dcm ipp (TODO: open PR and fix)
     nii2dcm_parameters = calc_ipp(nii[idx], nii2dcm_parameters)
-    
+
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         dicom = nii2dcm.dcm.DicomMRI("nii2dcm_dicom_mri.dcm")
@@ -116,7 +112,7 @@ def nifti2dicom(nii_paths: list[str], nii: list[Nifti1Image]) -> list[pydicom.Da
             keyword = keyword_for_tag(tag)
             if keyword in json[idx]:
                 setattr(dicom.ds, keyword, json[idx][keyword])
-                
+
     # Update with first instance params
     instance_index = 0
     with warnings.catch_warnings():
@@ -126,11 +122,15 @@ def nifti2dicom(nii_paths: list[str], nii: list[Nifti1Image]) -> list[pydicom.Da
 
     # Instance UID – unique to current slice
     dicom.ds.SOPInstanceUID = pydicom.uid.generate_uid(None)
-                
+
     # Get nifti data and header
-    nii_data = np.stack(img, axis=-1).T # (others, (phases), nz, ny, nx)
-    nii_head = {"json": json, "dset": dicom.ds, "ImagePositionPatient": nii2dcm_parameters["ImagePositionPatient"]}
-                
+    nii_data = np.stack(img, axis=-1).T  # (others, (phases), nz, ny, nx)
+    nii_head = {
+        "json": json,
+        "dset": dicom.ds,
+        "ImagePositionPatient": nii2dcm_parameters["ImagePositionPatient"],
+    }
+
     return nii_data, nii_head
 
     # Fill dicom list
@@ -174,40 +174,40 @@ def nifti2dicom(nii_paths: list[str], nii: list[Nifti1Image]) -> list[pydicom.Da
     #     # fix wrong nii2dcm ipp (TODO: open PR and fix)
     #     nii2dcm_parameters = calc_ipp(nii[idx], nii2dcm_parameters)
 
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter("ignore")
-        #     dicom = nii2dcm.dcm.DicomMRI("nii2dcm_dicom_mri.dcm")
-        #     transfer_nii_hdr_series_tags(dicom, nii2dcm_parameters)
-        #     dicom.ds.BitsAllocated = 32
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter("ignore")
+    #     dicom = nii2dcm.dcm.DicomMRI("nii2dcm_dicom_mri.dcm")
+    #     transfer_nii_hdr_series_tags(dicom, nii2dcm_parameters)
+    #     dicom.ds.BitsAllocated = 32
 
-        # # update from json
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter("ignore")
-        #     for tag in dicom.ds.keys():
-        #         keyword = keyword_for_tag(tag)
-        #         if keyword in json[idx]:
-        #             setattr(dicom.ds, keyword, json[idx][keyword])
+    # # update from json
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter("ignore")
+    #     for tag in dicom.ds.keys():
+    #         keyword = keyword_for_tag(tag)
+    #         if keyword in json[idx]:
+    #             setattr(dicom.ds, keyword, json[idx][keyword])
 
-        #     # update image type
-        #     vendor = dicom.ds.get("Manufacturer", "default")
-        #     if "GE" in vendor.upper():
-        #         dicom.ds.ImageType.insert(2, IMTYPE_MAPS[imtype[idx].name]["default"])
+    #     # update image type
+    #     vendor = dicom.ds.get("Manufacturer", "default")
+    #     if "GE" in vendor.upper():
+    #         dicom.ds.ImageType.insert(2, IMTYPE_MAPS[imtype[idx].name]["default"])
 
-        #     for instance_index in range(0, nii2dcm_parameters["NumberOfInstances"]):
-        #         transfer_nii_hdr_instance_tags(
-        #             dicom, nii2dcm_parameters, instance_index
-        #         )
-        #         setattr(dicom.ds, "InstanceNumber", instance_idx)
+    #     for instance_index in range(0, nii2dcm_parameters["NumberOfInstances"]):
+    #         transfer_nii_hdr_instance_tags(
+    #             dicom, nii2dcm_parameters, instance_index
+    #         )
+    #         setattr(dicom.ds, "InstanceNumber", instance_idx)
 
-        #         # Instance UID – unique to current slice
-        #         dicom.ds.SOPInstanceUID = pydicom.uid.generate_uid(None)
+    #         # Instance UID – unique to current slice
+    #         dicom.ds.SOPInstanceUID = pydicom.uid.generate_uid(None)
 
-        #         # Write pixel data
-        #         dicom.ds.FloatPixelData = img[idx][:, :, instance_index].tobytes()
+    #         # Write pixel data
+    #         dicom.ds.FloatPixelData = img[idx][:, :, instance_index].tobytes()
 
-        #         # append
-        #         dsets.append(copy.deepcopy(dicom.ds))
-        #         instance_idx += 1
+    #         # append
+    #         dsets.append(copy.deepcopy(dicom.ds))
+    #         instance_idx += 1
 
 
 def fnT1N(A, N):

src/mrbabel/io/converters/_nifti2mrd.py
@@ -13,14 +13,20 @@ import warnings
 import numpy as np
 import mrd
 
-from ._dicom2mrd import read_dicom_header, read_dicom_images, _get_unique_contrasts, IMTYPE_MAPS
+from ._dicom2mrd import (
+    read_dicom_header,
+    read_dicom_images,
+    _get_unique_contrasts,
+    IMTYPE_MAPS,
+)
+
 
 def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
     """Create MRD Header from a NIfTI file."""
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         head = read_dicom_header(nii_head["dset"])
-    
+
     # Get sequence parameters from json
     flip_angle_deg = [float(json["FlipAngle"]) for json in nii_head["json"]]
     t_r = [float(json["RepetitionTime"]) * 1000.0 for json in nii_head["json"]]
@@ -29,7 +35,7 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
         t_i = [float(json["InversionTime"]) * 1000.0 for json in nii_head["json"]]
     else:
         t_i = []
-        
+
     # Get unique contrasts
     if len(t_i) > 0:
         contrasts = np.stack([flip_angle_deg, t_r, t_e, t_i], axis=1)
@@ -39,7 +45,7 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
         contrasts = np.stack([flip_angle_deg, t_r, t_e], axis=1)
         u_contrasts, contrast_idx = _get_unique_contrasts(contrasts)
         flip_angle_deg, t_r, t_e = u_contrasts
-        
+
     # Remove duplicates
     n_contrasts = len(flip_angle_deg)
     flip_angle_deg = np.unique(flip_angle_deg).tolist()
@@ -47,7 +53,7 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
     t_r = np.unique(t_r).tolist()
     if len(t_i) > 0:
         t_i = np.unique(t_i).tolist()
-            
+
     # Update sequence parameters
     if head.sequence_parameters is None:
         head.sequence_parameters = mrd.SequenceParametersType()
@@ -55,7 +61,7 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
     head.sequence_parameters.t_r = t_r
     head.sequence_parameters.t_e = t_e
     head.sequence_parameters.t_i = t_i
-    
+
     # Update Z fov and matrix size
     n_slices = len(nii_head["ImagePositionPatient"])
     head.encoding[-1].encoded_space.field_of_view_mm.z *= n_slices
@@ -63,26 +69,28 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
 
     head.encoding[-1].recon_space.field_of_view_mm.z *= n_slices
     head.encoding[-1].recon_space.matrix_size.z *= n_slices
-    
+
     # Update encoding limits
     head.encoding[-1].encoding_limits.slice.maximum = n_slices - 1
     head.encoding[-1].encoding_limits.slice.center = n_slices // 2
 
     head.encoding[-1].encoding_limits.contrast.maximum = n_contrasts - 1
     head.encoding[-1].encoding_limits.contrast.center = n_contrasts // 2
-    
+
     # Get vendor
     vendor = nii_head["json"][0]["Manufacturer"]
+
     def get_image_type(item):
         if "GE" in vendor.upper():
             try:
                 return IMTYPE_MAPS["default"][item[3][0]]
             except Exception:
                 return mrd.ImageType.MAGNITUDE
-        return IMTYPE_MAPS["default"][item[2][0]]    
+        return IMTYPE_MAPS["default"][item[2][0]]
+
     image_types = [get_image_type(json["ImageType"]).value for json in nii_head["json"]]
     image_types = np.asarray(image_types)
-    
+
     # Sort images and jsons
     n_volumes = nii_data.shape[0]
 
@@ -98,40 +106,46 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
         _json = []
         _contrast_idx = []
         if sum(image_types == 1) > 0:
-            _nii_data[0] = np.zeros((sum(image_types == 1), *nii_data[0].shape), dtype=np.float32)
+            _nii_data[0] = np.zeros(
+                (sum(image_types == 1), *nii_data[0].shape), dtype=np.float32
+            )
         if sum(image_types == 2) > 0:
-            _nii_data[1] = np.zeros((sum(image_types == 2), *nii_data[0].shape), dtype=np.float32)
+            _nii_data[1] = np.zeros(
+                (sum(image_types == 2), *nii_data[0].shape), dtype=np.float32
+            )
         if sum(image_types == 3) > 0:
-            _nii_data[2] = np.zeros((sum(image_types == 3), *nii_data[0].shape), dtype=np.float32)
+            _nii_data[2] = np.zeros(
+                (sum(image_types == 3), *nii_data[0].shape), dtype=np.float32
+            )
         if sum(image_types == 4) > 0:
-            _nii_data[3] = np.zeros((sum(image_types == 4), *nii_data[0].shape), dtype=np.float32)
+            _nii_data[3] = np.zeros(
+                (sum(image_types == 4), *nii_data[0].shape), dtype=np.float32
+            )
 
         # Sorting
         for n in range(n_volumes):
             idx = image_types[n] - 1
             cidx = contrast_idx[n]
             _nii_data[idx][cidx] = nii_data[n].astype(np.float32)
-            if idx == 0: # magnitude
+            if idx == 0:  # magnitude
                 _json.append(nii_head["json"][n])
                 _contrast_idx.append(cidx)
-            
+
         # Real + 1j * Imag
         if sum(image_types == 3) > 0 and sum(image_types == 4) > 0:
             nii_data = _nii_data[2] + 1j * _nii_data[3]
         else:
-            if (_nii_data[1] > 2 * np.pi).any():
-                mag = _nii_data[0].view()
-                phase = _nii_data[1].view()
-                phase = phase / 4096.0
-                nii_data = _nii_data[0] * np.exp(1j * (2 * np.pi * _nii_data[1] / 4095))
-            else:
-                nii_data = _nii_data[0] * np.exp(1j * _nii_data[1])
-                
+            mag = _nii_data[0].view()
+            phase = _nii_data[1].view()
+            phase = (phase - phase.min()) / (phase.max() - phase.min())
+            phase = 2 * np.pi * phase - np.pi
+            nii_data = mag * np.exp(1j * phase)
+
     # Sort contrasts
     _order = np.argsort(_contrast_idx)
     nii_data = nii_data[_order]
     _json = np.asarray(_json)[_order].tolist()
-           
+
     # Correct phase shift along z for GE systems
     if (
         is_complex
@@ -139,33 +153,38 @@ def read_nifti_header(nii_data: np.ndarray, nii_head: dict) -> mrd.Header:
         and head.acquisition_system_information.system_vendor
     ):
         if "GE" in head.acquisition_system_information.system_vendor.upper():
-            nii_data = np.fft.ifft(
-                np.fft.fftshift(np.fft.fft(nii_data, axis=-3), axes=-3), axis=-3
-            )
-    
+            mag = np.abs(nii_data.view())
+            phase = np.angle(nii_data.view())
+            phase[..., 1::2, :, :] = (
+                (1e5 * (phase[..., 1::2, :, :] + 2 * np.pi)) % (2 * np.pi * 1e5)
+            ) / 1e5 - np.pi
+            nii_data = mag * np.exp(1j * phase)
+
     # Enforce single precision
     nii_data = nii_data.astype(np.complex64)
-    
+
     # Assign json
     nii_head["json"] = _json
-            
+
     return nii_data, nii_head, head
-    
-    
-def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> mrd.ImageArray:
+
+
+def read_nifti_image(
+    nii_data: np.ndarray, nii_head: dict, head: mrd.Header
+) -> mrd.ImageArray:
     """Create MRD ImageArray from a NIfTI file."""
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         image_template, _ = read_dicom_images([nii_head["dset"]], copy.deepcopy(head))
-    
+
     # Get image_header and image_meta templates
     image_head_template = image_template[0].head
     image_meta_template = image_template[0].meta
     image_meta_template.pop("DicomJson", None)
-    
+
     # Get image position patient
     ipp = nii_head["ImagePositionPatient"]
-    
+
     # Get sizes
     n_contrasts = nii_data.shape[0]
     if len(nii_data.shape) == 5:
@@ -174,11 +193,11 @@ def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> 
     else:
         n_phases = 1
         n_slices = nii_data.shape[1]
-    
+
     # Construct headers
     image_head = np.empty((n_contrasts, n_phases, n_slices), dtype=object)
     image_meta = np.empty((n_contrasts, n_phases, n_slices), dtype=object)
-    
+
     # Construction
     for contrast_idx in range(n_contrasts):
         for phase_idx in range(n_phases):
@@ -190,7 +209,7 @@ def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> 
                 _image_head.contrast = contrast_idx
                 _image_head.phase = phase_idx
                 _image_head.slice = slice_idx
-                
+
                 # Flags
                 defs = mrd.ImageFlags
                 if contrast_idx == 0:
@@ -205,13 +224,13 @@ def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> 
                     _image_head.flags = defs.FIRST_IN_SLICE
                 if slice_idx == n_slices - 1:
                     _image_head.flags = defs.LAST_IN_SLICE
-                    
+
                 image_head[contrast_idx, phase_idx, slice_idx] = _image_head
-                
+
                 # image meta
                 _image_meta = copy.deepcopy(image_meta_template)
                 image_meta[contrast_idx, phase_idx, slice_idx] = _image_meta
-                
+
     # Transpose
     if len(nii_data.shape) == 5:
         nii_data = nii_data.swapaxes(0, 1)
@@ -219,12 +238,12 @@ def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> 
         nii_data = nii_data[None, ...]
     nii_data = np.flip(nii_data, (-2, -1))
     nii_data = np.ascontiguousarray(nii_data)
-    
+
     # assign
     image_data = nii_data.view()
     image_head = image_head.swapaxes(0, 1)
     image_meta = image_meta.swapaxes(0, 1)
-    
+
     # Add axis map
     axis_map_keys = ["phase", "contrast", "slice", "rows", "columns"]
     axis_map_keys = np.asarray(axis_map_keys)
@@ -238,15 +257,12 @@ def read_nifti_image(nii_data: np.ndarray, nii_head: dict, head: mrd.Header) -> 
     head.user_parameters.user_parameter_base64.append(
         mrd.UserParameterBase64Type(name="AxisMaps", value=axis_map)
     )
- 
+
     return (
         mrd.ImageArray(
-            data=image_data.squeeze(), headers=image_head.squeeze(), meta=image_meta.squeeze()
+            data=image_data.squeeze(),
+            headers=image_head.squeeze(),
+            meta=image_meta.squeeze(),
         ),
         head,
     )
-    
-    
-    
-    
-   

src/mrbabel/io/sorting/_sort_images.py
@@ -32,7 +32,7 @@ def sort_images(images: list[mrd.Image], head: mrd.Header) -> mrd.ImageArray:
     _meta = np.asarray([img.meta for img in images])
     _headers = np.asarray([img.head for img in images])
     _data = np.stack([img.data for img in images])
-    _data = _data.astype(np.float32) # cast to single precision
+    _data = _data.astype(np.float32)  # cast to single precision
 
     # Get slice idx
     slice_idx = np.asarray([head.slice for head in _headers])
@@ -133,10 +133,11 @@ def sort_images(images: list[mrd.Image], head: mrd.Header) -> mrd.ImageArray:
         if sum(image_types == 3) > 0 and sum(image_types == 4) > 0:
             data = data[2] + 1j * data[3]
         else:
-            if (data[1] > 2 * np.pi).any():
-                data = data[0] * np.exp(1j * (2 * np.pi * data[1] / 4095 - np.pi))
-            else:
-                data = data[0] * np.exp(1j * data[1])
+            mag = data[0].view()
+            phase = data[1].view()
+            phase = (phase - phase.min()) / (phase.max() - phase.min())
+            phase = 2 * np.pi * phase - np.pi
+            data = mag * np.exp(1j * phase)
 
     # Correct phase shift along z for GE systems
     if (
@@ -145,10 +146,13 @@ def sort_images(images: list[mrd.Image], head: mrd.Header) -> mrd.ImageArray:
         and head.acquisition_system_information.system_vendor
     ):
         if "GE" in head.acquisition_system_information.system_vendor.upper():
-            data = np.fft.ifft(
-                np.fft.fftshift(np.fft.fft(data, axis=-3), axes=-3), axis=-3
-            )
-            
+            mag = np.abs(data.view())
+            phase = np.angle(data.view())
+            phase[..., 1::2, :, :] = (
+                (1e5 * (phase[..., 1::2, :, :] + 2 * np.pi)) % (2 * np.pi * 1e5)
+            ) / 1e5 - np.pi
+            data = mag * np.exp(1j * phase)
+
     # Enforce single precision
     data = data.astype(np.complex64)
 

src/mrbabel/io/sorting/_sort_kspace.py
@@ -58,12 +58,12 @@ def sort_kspace(
     axis_maps = []
     for n in range(n_encoded_spaces):
         data = np.stack([d for d in _data[n]])
-        data = data.astype(np.float32) # cast to single precision
+        data = data.astype(np.float32)  # cast to single precision
         try:
             trajectory = np.stack([traj for traj in _trajectory[n]])
-            trajectory = trajectory.astype(np.float32) # cast to single precision
+            trajectory = trajectory.astype(np.float32)  # cast to single precision
             density = np.stack([dens for dens in _density[n]])
-            density = density.astype(np.float32) # cast to single precision
+            density = density.astype(np.float32)  # cast to single precision
         except Exception:
             trajectory = np.asarray([])
             density = np.asarray([])